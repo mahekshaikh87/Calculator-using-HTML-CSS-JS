63a295dec96f4edec4476802e8f1b23f2002396e
mahekshaikh87 <mahekshaikh878787@gmail.com>
2026-01-11 13:12:13 +0530

    commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let x = document.getElementById("display");
+let buttons = document.querySelectorAll("button");
+let boxContent ="";
+
+buttons.forEach( function(button){
+	button.addEventListener("click",function(){
+		const value=button.textContent;
+
+		if(value==="="){
+			boxContent=eval(boxContent);
+		}
+		else if ( value==="C"){
+			boxContent="";
+		}
+		else {
+			boxContent +=value;
+		}
+
+        x.value=boxContent;
+	})
+})
+
+
